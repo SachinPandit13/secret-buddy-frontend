a38a048561b5565f56335b86724f015e321fac8e
Sachin Pandit <sachininfo13@gmail.com>
2025-07-20 17:21:08 +0530

    updated the shared index path

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,2 +1,2 @@
-export * from './header/header.component';
-export * from './tabs/tabs.component';
+export * from './components/header/header.component';
+export * from './components/tabs/tabs.component';
